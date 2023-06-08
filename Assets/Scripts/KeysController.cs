87eb977181f747f13dd685db21bab6b81a1a594f
André <a.silva14@hotmail.com>
2023-06-08 15:25:33 +0200

    Midi Inpute implemented and keyboard working

--- Assets/Scripts/KeysController.cs
+++ Assets/Scripts/KeysController.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using MidiJack;
 using UnityEngine;
 
 public class KeysController : MonoBehaviour
 {
+    private SpriteRenderer _spRend;
+    public Sprite KeysDefault;
+
+    public Sprite KeysPressed;
+    
+    public MidiChannel keyToPress;
+
+    public int noteNumber;
+    
     // Start is called before the first frame update
     void Start()
     {
+        _spRend = GetComponent<SpriteRenderer>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
+      if (MidiDriver.Instance.GetKeyDown(keyToPress, noteNumber))
+      {
+          _spRend.sprite = KeysPressed;
+      }
+      
+      if (MidiDriver.Instance.GetKeyUp(keyToPress, noteNumber))
+      {
+          _spRend.sprite = KeysDefault;
+      }
         
     }
 }
